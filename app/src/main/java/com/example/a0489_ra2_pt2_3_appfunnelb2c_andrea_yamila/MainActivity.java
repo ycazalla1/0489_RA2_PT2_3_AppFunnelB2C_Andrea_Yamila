7667987cd1293bd6508c9fefa25159f882cf85cc
Ann <aesplugas@insgabrielamistral.cat>
2025-10-16 18:12:44 +0200

    MainActivity Fons casi fet

--- app/src/main/java/com/example/a0489_ra2_pt2_3_appfunnelb2c_andrea_yamila/MainActivity.java
+++ app/src/main/java/com/example/a0489_ra2_pt2_3_appfunnelb2c_andrea_yamila/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a0489_ra2_pt2_3_appfunnelb2c_andrea_yamila;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        VideoView videoView = findViewById(R.id.videoView);
+
+        // Ruta del video
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.fons_panatalla_prova;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+
+        // Controles de reproducción
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        // Iniciar video
+        videoView.start();
+
+        // Listener cuando termine el video
+        videoView.setOnCompletionListener(mp -> {
+            // Ir a la siguiente Activity
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+            finish(); // Opcional: cerrar la Activity actual
         });
     }
 }
