ad2b397d3171b40ea96771bfdb3cee064b9355f5
ycazalla1 <ycazaa1@gmail.com>
2025-10-16 16:09:13 +0200

    Activities creadas

---
+++ app/src/main/java/com/example/a0489_ra2_pt2_3_appfunnelb2c_andrea_yamila/MainActivity6.java
@@ -0,0 +1,24 @@
+package com.example.a0489_ra2_pt2_3_appfunnelb2c_andrea_yamila;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main6);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
